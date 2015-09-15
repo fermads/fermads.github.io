8ca81200c9b75937a6b182c78363ab5cd8dd6343
Fernando MAdS <fermads@gmail.com>
2015-09-15 00:19:00 -0300

    adding twitter and npm

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var maskedMail = document.getElementById('masked-mail');
+'use strict';
 
-maskedMail.addEventListener('mouseover', showMail);
-maskedMail.addEventListener('click', showMail);
+var maskedMail = document.getElementById('masked-mail');
+var arrowDown = document.querySelector('.arrow-down');
+
+if (maskedMail) {
+  maskedMail.addEventListener('mouseover', showMail);
+  maskedMail.addEventListener('click', showMail);
+}
+
+if (arrowDown) {
+  arrowDown.addEventListener('click', function() {
+    location.href = '#timeline';
+  });
+}
 
 function showMail() {
   maskedMail.setAttribute('href', 'mailto:fermads?'.replace('?', '@gmail.com'));
